601ef4761383616f77853af6420369863dda7ea0
Leon <m.dixon5030@gmail.com>
2026-02-15 16:48:26 -0500

    Move Property Coverage Dashboard to admin panel (/admin/property-coverage)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -30,7 +30,7 @@ import { MapLayersProvider } from './contexts/MapLayersContext';
 import { ShowcaseLandingPage } from './pages/ShowcaseLandingPage';
 import { DealShowcasePage } from './pages/DealShowcasePage';
 import { ModuleShowcasePage } from './pages/ModuleShowcasePage';
-import { PropertyCoveragePage } from './pages/PropertyCoveragePage';
+import { PropertyCoveragePage } from './pages/admin/PropertyCoveragePage';
 
 
 function AppContent() {
@@ -70,7 +70,7 @@ function AppContent() {
           <Route path="/assets-owned/documents" element={<Navigate to="/assets-owned" replace />} />
           <Route path="/assets-owned/grid" element={<Navigate to="/assets-owned" replace />} />
           <Route path="/properties" element={<PropertiesPage />} />
-          <Route path="/property-coverage" element={<PropertyCoveragePage />} />
+          <Route path="/admin/property-coverage" element={<PropertyCoveragePage />} />
           <Route path="/deals" element={<DealsPage />} />
           <Route path="/deals/create" element={<CreateDealPage />} />
           <Route path="/deals/kanban" element={<Navigate to="/deals" replace />} />
